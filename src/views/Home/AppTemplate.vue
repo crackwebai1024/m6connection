f7aa6774835244c2f98a3c82ba6b3482734075c2
crackwebai1024 <webtowardapex@outlook.com>
2021-01-21 01:34:14 +0800

    almost complete

--- src/views/Home/AppTemplate.vue
+++ src/views/Home/AppTemplate.vue
@@ -4,18 +4,14 @@
       <slot name="actionbtn" />
     </div>
     <template>
-      <v-row
-        v-if="dialog"
-        class="dont-show-scroll mx-0 vertical-scroll"
-      >
-        <v-card
-          class="grey lighten-3 relative w-full"
-          tile
-        >
+      <v-row v-if="dialog" class="dont-show-scroll mx-0 vertical-scroll">
+        <v-card class="grey lighten-3 relative w-full" tile>
           <div class="w-full white">
             <slot name="header" />
             <v-divider class="blue-grey lighten-5 max-w-lg mx-auto w-full" />
-            <div class="align-center d-flex justify-space-between max-w-lg mx-auto py-1 w-full">
+            <div
+              class="align-center d-flex justify-space-between max-w-lg mx-auto py-1 w-full"
+            >
               <slot name="tabs" />
               <v-spacer />
               <slot name="btns" />
@@ -24,7 +20,9 @@
           <div
             class="details-content grey h-fit lighten-3 min-h-full pt-3 relative"
           >
-            <v-row class="align-start d-flex justify-space-between max-w-lg mx-auto pt-1 w-full">
+            <v-row
+              class="align-start d-flex justify-space-between max-w-lg mx-auto pt-1 w-full"
+            >
               <slot name="content" />
             </v-row>
             <v-badge
@@ -33,10 +31,14 @@
               offset-x="17"
               offset-y="17"
               overlap
-              :style=" actionOverlay ? 'z-index: 203;' : '' "
+              :style="actionOverlay ? 'z-index: 203;' : ''"
             >
               <v-btn
-                :class="actionOverlay ? 'white ml-auto pa-6 white--text' : 'darken-2 grey ml-auto pa-6 white--text' "
+                :class="
+                  actionOverlay
+                    ? 'white ml-auto pa-6 white--text'
+                    : 'darken-2 grey ml-auto pa-6 white--text'
+                "
                 :color="!actionOverlay ? 'white' : 'grey darken-2'"
                 depressed
                 icon
@@ -45,9 +47,7 @@
               >
                 <v-icon>mdi-menu</v-icon>
               </v-btn>
-              <template v-slot:badge>
-                12
-              </template>
+              <template v-slot:badge> 12 </template>
             </v-badge>
             <v-overlay
               class="record-overlay"
@@ -58,14 +58,8 @@
                 class="central-content flex flex-nowrap justify-space-between mx-auto relative top-6 transparent0 w-full"
                 no-gutters
               >
-                <v-col
-                  class="dont-show-scroll vertical-scroll"
-                  cols="3"
-                >
-                  <div
-                    class="w-full"
-                    style="height: 310px;"
-                  />
+                <v-col class="dont-show-scroll vertical-scroll" cols="3">
+                  <div class="w-full" style="height: 310px" />
                   <action-feed :light-mode="true" />
                 </v-col>
               </v-row>
@@ -86,21 +80,13 @@
         >
           <v-icon>mdi-message</v-icon>
         </v-btn>
-        <v-overlay
-          class="record-overlay"
-          opacity="0.7"
-          :value="chatOverlay"
-        >
+        <v-overlay class="record-overlay" opacity="0.7" :value="chatOverlay">
           <v-row
             class="central-content flex flex-nowrap justify-space-between mx-auto relative top-6 transparent0 w-full"
             no-gutters
           >
-            <v-col
-              cols="9"
-            />
-            <div
-              class="relative w-side"
-            >
+            <v-col cols="9" />
+            <div class="relative w-side">
               <v-btn
                 absolute
                 class="blue--text ml-auto pa-6 white"
@@ -128,42 +114,42 @@
 </template>
 
 <script>
-import ActionFeed from '@/views/Home/ActionFeed'
-import M6Chat from '@/components/Home/M6Chat'
+import ActionFeed from "@/views/Home/ActionFeed";
+import M6Chat from "@/components/Home/M6Chat";
 export default {
-  name: 'AppTemplate',
+  name: "AppTemplate",
   components: {
     ActionFeed,
-    M6Chat
+    M6Chat,
   },
   props: {
     propsDialog: {
       type: Boolean,
-      default: false
+      default: false,
     },
     isPersistent: {
       type: Boolean,
-      default: false
+      default: false,
     },
     openDialog: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
   data: () => ({
     dialog: false,
     chatOverlay: false,
-    actionOverlay: false
+    actionOverlay: true,
   }),
   watch: {
     propsDialog(val) {
-      this.dialog = val
-    }
+      this.dialog = val;
+    },
   },
   created() {
-    this.dialog = this.openDialog
-  }
-}
+    this.dialog = this.openDialog;
+  },
+};
 </script>
 
 <style lang="scss">
@@ -194,14 +180,14 @@ export default {
   width: 0px;
 }
 .add-field .v-input input {
-    border-bottom: 1px solid #F8F3EC;
-    margin-bottom: -1px;
+  border-bottom: 1px solid #f8f3ec;
+  margin-bottom: -1px;
 }
 .panel {
-    min-height: 300px;
+  min-height: 300px;
 }
 .v-tabs-slider {
-    color: #2196F3;
+  color: #2196f3;
 }
 .record-overlay .v-overlay__content {
   width: 100%;
